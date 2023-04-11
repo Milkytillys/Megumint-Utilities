c261403e3fc2d4c022da176d308917dce352bc16
Megumint <66193126+Poopland@users.noreply.github.com>
2023-04-12 02:44:37 +0700

    Delete SYN-ClearSignal.lua

--- SYN-ClearSignal.lua
+++
@@ -1,8 +0,0 @@
-function ClearSignal(Connection,RobloxConnection)
-    for i,v in pairs(getconnections(Connection)) do
-        if v.Function and (RobloxConnection or (isexecutorclosure or is_synapse_function)(v.Function)) then
-            v:Disable()
-        end
-    end
-end
-return ClearSignal
